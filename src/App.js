334db9b1d409b7746ca9b3310c7d166b348611e5
mablds <marcelobragalemos@gmail.com>
2020-03-10 22:34:27 -0300

    handleChanges and loginHAndler added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import './App.css';
+const axios = require('axios');
 
 class App extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+
     this.state = {
-      
+      account: "",
+      password: ""
     }
+  }
+
+  loginHandler = async (event) => {
+    event.preventDefault();
+    axios.post('http://simple-bank.herokuapp.com/auth', {
+      header: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: {
+        account: this.state.account,
+        password: this.state.password
+      }
+    }).then((res) => console.log(res));
+  }
+
+  handleChange = event => {
+    const { value, name } = event.target;
+    this.setState({ [name]: value});
   }
 
   render() {
     return (
       <div className="App">
-        <div className="container">
+        <form className="container" onSubmit={this.loginHandler}>
           <h2>Bem vindo de volta!</h2>
-            <input type="text" name="account" placeholder="Account"/>
-            <input type="password" name="account-password" placeholder="Password"/>
-            <button id="submit" type="submit" value="Acessar">Acessar</button>
-        </div>
+            <input type="text" name="account" placeholder="Account" onChange={this.handleChange} />
+            <input type="password" name="account-password" placeholder="Password" onChange={this.handleChange}/>
+            <button id="submit" type="submit" value="Acessar"  >Acessar</button>
+        </form>
       </div>
     );
   }
