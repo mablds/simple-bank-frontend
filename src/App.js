addc36f57ef8c43fefe092887b21277baa158c35
mablds <marcelobragalemos@gmail.com>
2020-04-21 15:16:36 -0300

    Saldo component updated verifying props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Route } from 'react-router-dom'
-import Login from './pages/login/login.component'
+import { Route, BrowserRouter } from 'react-router-dom'
+
+import Login from './pages/login/login.component';
+import Saldo from './components/saldo/saldo.component';
 import './App.css';
 
 function App() {
     return (
-      <div>
-        <Route exact path='/' component={Login} />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Route exact path='/' component={Login} />
+          <Route exact path='/main-menu' component={Saldo} />
+        </div>
+      </BrowserRouter>
     );
 }
-
 export default App;
---
+++ src/components/saldo/saldo.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './saldo.styles.scss'
+
+const Login = () => (
+    <div>
+      <h1>hahahah</h1>
+    </div>
+  )
+
+  export default Login
