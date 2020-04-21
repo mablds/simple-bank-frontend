01e040121b28416fc7499b8f11b6e5083f1d7ea6
mablds <marcelobragalemos@gmail.com>
2020-04-21 01:54:50 -0300

    break to login component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
+import Login from './pages/login/login.component'
 import './App.css';
-const axios = require('axios');
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      email: "",
-      password: "",
-      accountInfo: {}
-    }
-  }
-
-  loginHandler = async (event) => {
-    //http://simple-bank.herokuapp.com/auth
-    event.preventDefault();
-    axios.post('http://simple-bank.herokuapp.com/auth', {
-        email: this.state.email ,
-        password: this.state.password
-      }
-    ).then((res) => {
-      if(res.status === 200) {
-        this.state.accountInfo = res.data;
-        console.log(this.state.accountInfo)
-      }
-    })
-  }
-
-  handleChange = event => {
-    const { value, name } = event.target;
-    this.setState({ [name]: value});
-  }
-
-  render() {
+function App() {
     return (
-      <div className="App">
-        <form className="container" onSubmit={this.loginHandler}>
-          <h2>Bem vindo de volta!</h2>
-            <input type="text" name="email" placeholder="Enter your email" onChange={this.handleChange} />
-            <input type="password" name="password" placeholder="Type your very secure password" onChange={this.handleChange}/>
-            <button id="submit" type="submit" value="Acessar">Acessar</button>
-        </form>
+      <div>
+        <Route exact path='/' component={Login} />
       </div>
     );
-  }
-
 }
 
 export default App;
---
+++ src/pages/login/login.component.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import './login.styles.scss'
+
+const axios = require('axios');
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: "",
+            accountInfo: {}
+        }
+    }
+
+    loginHandler = async (event) => {
+        //http://simple-bank.herokuapp.com/auth
+        event.preventDefault();
+        axios.post('http://simple-bank.herokuapp.com/auth', {
+            email: this.state.email ,
+            password: this.state.password
+          }
+    ).then((res) => {
+        if(res.status === 200) {
+            this.state.accountInfo = res.data;
+            console.log(this.state.accountInfo)
+          }
+        })
+    }
+    
+    handleChange = event => {
+        const { value, name } = event.target;
+        this.setState({ [name]: value});
+    }
+    
+    render() {
+        return (
+            <div className="App">
+                <form className="container" onSubmit={this.loginHandler}>
+                <h2>Bem vindo de volta!</h2>
+                    <input type="text" name="email" placeholder="Enter your email" onChange={this.handleChange} />
+                    <input type="password" name="password" placeholder="Type your very secure password" onChange={this.handleChange}/>
+                    <button id="submit" type="submit" value="Acessar">Acessar</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Login;
