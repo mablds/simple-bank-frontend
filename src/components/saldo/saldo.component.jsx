17ecf6f5089dc470f06ff78556f660b553ee4e75
mablds <marcelobragalemos@gmail.com>
2020-08-06 16:26:56 -0300

    update sequence of userLgogedIn obj declaration

--- src/components/saldo/saldo.component.jsx
+++ src/components/saldo/saldo.component.jsx
@@ -5,18 +5,21 @@ import './saldo.styles.scss'
 import eye from '../../public-imgs/icons/eye.svg';
 import { useState } from 'react';
 
-const userLoggedIn = JSON.parse(localStorage.getItem('login'));
 
 const Saldo = () => {
+    const userLoggedIn = JSON.parse(localStorage.getItem('login'));
     const [ isVisible, setIsVisible ] = useState(false);
-
+    
     return (
         <div id="saldo">
             <div id="owner">
-                Olá, {userLoggedIn.owner}!
+                <span>Olá, { userLoggedIn.owner }!</span>
+            </div>
+            <div id="account-info">
+                <span>Conta: { userLoggedIn.account }</span>
             </div>
             <div id="valor">
-                <span>{isVisible ? userLoggedIn.value : `--------`}</span>
+                <span>{ isVisible ? userLoggedIn.value : `--------` }</span>
                 <img className="icon" id="eye" src={eye} alt="visible function" onClick={ () => setIsVisible(!isVisible) }/>
             </div>
         </div>
